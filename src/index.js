31b394f40eabba193bc86b84864e43255799b9f6
joshlikesyou@gmail.com <joshlikesyou@gmail.com>
2017-05-30 11:40:26 -0700

    added script build for heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementByClass('container'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
